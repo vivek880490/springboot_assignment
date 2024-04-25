d69803fd9a083e9fc21880e2bd575bd2d9d70472
vivek kumar <vivek880490@gmail.com>
2024-04-25 17:37:21 +0530

    setup application

---
+++ src/main/java/com/example/springboot_assignment/UserController.java
@@ -0,0 +1,28 @@
+package com.example.springboot_assignment;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    @GetMapping
+    public String getUser(){
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "create user was called";
+
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "update user was called";
+    }
+
+    public String deleteUser(){
+        return "delete user was called";
+    }
+}
