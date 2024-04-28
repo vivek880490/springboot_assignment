9537070a7a313dd2a8e9d73a3d5131628d2161e3
vivek kumar <vivek880490@gmail.com>
2024-04-28 18:39:01 +0530

    custom error message

--- src/main/java/com/example/springboot_assignment/AppExceptionHandler.java
+++ src/main/java/com/example/springboot_assignment/AppExceptionHandler.java
@@ -9,12 +9,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(value = {Exception.class})
+
     public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request){
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if(errorMessageDescription == null) errorMessageDescription = ex.toString();
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
         return new ResponseEntity<>(
-                ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+                errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 }
--- src/main/java/com/example/springboot_assignment/ErrorMessage.java
+++ src/main/java/com/example/springboot_assignment/ErrorMessage.java
@@ -1,4 +1,29 @@
 package com.example.springboot_assignment;
 
+import java.util.Date;
+
 public class ErrorMessage {
+    private String message;
+    private Date date;
+
+    public ErrorMessage(Date date, String message){
+        this.date = date;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
